79480abc4a5f881a2d54ce47be4c2eeaba259f7a
Yang Hanlin <mattoncis@hotmail.com>
2021-01-03 19:47:49 +0800

    feat: add request ID to each callback request

--- app/controller/callback/home.js
+++ app/controller/callback/home.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const { Controller } = require('egg');
+const { v4: uuid } = require('uuid');
 
 const CONTROLLERS_PER_EVENT = {};
 
 class CallbackController extends Controller {
   async index() {
     const { ctx, app } = this;
+    const requestId = uuid();
+    ctx.requestId = requestId;
     const expectedToken = app.config.webhookSecretToken;
     if (expectedToken) {
       const token = ctx.get('X-Gitlab-Token');
       if (token !== expectedToken) {
         ctx.body = {
           error: 'Incorrect webhook secret token',
+          requestId,
         };
         ctx.status = 403;
         return;
@@ -22,6 +26,7 @@ class CallbackController extends Controller {
     if (!eventType) {
       ctx.body = {
         error: 'Missing event type',
+        requestId,
       };
       ctx.status = 400;
       return;
@@ -30,6 +35,7 @@ class CallbackController extends Controller {
     if (!SpecificController) {
       ctx.body = {
         error: `Handler for event type '${eventType}' is not found`,
+        requestId,
       };
       ctx.status = 404;
     } else {
