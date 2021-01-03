7b2345cdf68d01edc3ae2f1e11e337f4efe873c7
Yang Hanlin <mattoncis@hotmail.com>
2021-01-03 23:25:56 +0800

    feat: implement auto-comment when mentioned

--- app/service/events/issue.js
+++ app/service/events/issue.js
@@ -8,6 +8,20 @@ class IssueService extends Service {
     if (await ctx.service.utilities.checkBotBanned()) {
       return;
     }
+    ctx.runInBackground(async ctx => {
+      const { app } = this;
+      const { username } = await app.gitbeaker.Users.current();
+      const { action, iid: issueId } = ctx.request.body.object_attributes;
+      const projectId = ctx.request.body.project.id;
+      const senderUsername = ctx.request.body.user.username;
+      if (action === 'open' || action === 'update') {
+        const { title, description } = ctx.request.body.object_attributes;
+        const regex = new RegExp(`@${username}`);
+        if (title.match(regex) || description.match(regex)) {
+          app.gitbeaker.IssueNotes.create(projectId, issueId, `@${senderUsername} 你吼那么大声干吗嘛`);
+        }
+      }
+    });
   }
 }
 
